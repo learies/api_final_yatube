ec857631ae7291330e5fc9858a87ce810d841592
LeAries <learies@ya.ru>
2022-04-19 20:25:05 +0300

    Delete group for PostSerializer

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -6,7 +6,6 @@ from posts.models import Post
 
 class PostSerializer(serializers.ModelSerializer):
     author = SlugRelatedField(slug_field='username', read_only=True)
-    group = serializers.StringRelatedField()
 
     class Meta:
         fields = ('id', 'text', 'pub_date', 'author', 'group')
