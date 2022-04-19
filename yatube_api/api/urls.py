f438a0a9453a04b5764a6b13d35da1f7c6685fe1
LeAries <learies@ya.ru>
2022-04-19 14:57:28 +0300

    Add api PostViewSet

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,12 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from .views import PostViewSet
+
+v1_router = DefaultRouter()
+
+v1_router.register('posts', PostViewSet, basename='post')
 
 urlpatterns = [
+    path('v1/', include(v1_router.urls)),
 ]
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,16 @@
-# TODO:  Напишите свой вариант
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+from posts.models import Post
+
+from .permissions import IsAuthorOrReadOnly
+from .serializers import PostSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly)
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
