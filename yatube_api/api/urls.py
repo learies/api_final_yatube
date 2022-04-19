582084120a4959761712364a73f67348b2b0544e
LeAries <learies@ya.ru>
2022-04-19 20:59:19 +0300

    Add djoser

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -8,5 +8,6 @@ v1_router = DefaultRouter()
 v1_router.register('posts', PostViewSet, basename='post')
 
 urlpatterns = [
+    path('v1/', include('djoser.urls.jwt')),
     path('v1/', include(v1_router.urls)),
 ]
