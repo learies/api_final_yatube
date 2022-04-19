0576eac064240a08f4719baf222211add019d001
LeAries <learies@ya.ru>
2022-04-19 15:47:54 +0300

    Add in admin Post and Group

---
+++ yatube_api/posts/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+
+from posts.models import Post, Group
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'text',
+        'pub_date',
+        'author',
+    )
+
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+    )
