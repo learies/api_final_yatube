f85b35f7cb3b0c7ee27a54ed697ab9ad8b033ed2
LeAries <learies@ya.ru>
2022-04-19 21:35:01 +0300

    Add Follow and Comment in admin

--- yatube_api/posts/admin.py
+++ yatube_api/posts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from posts.models import Post, Group
+from posts.models import Comment, Follow, Group, Post
 
 
 @admin.register(Post)
@@ -18,4 +18,22 @@ class PostAdmin(admin.ModelAdmin):
 class GroupAdmin(admin.ModelAdmin):
     list_display = (
         'title',
+    )
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = (
+        'text',
+        'post',
+        'author',
+        'created',
+    )
+
+
+@admin.register(Follow)
+class FollowAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'following',
     )
