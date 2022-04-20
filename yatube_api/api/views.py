80f62752ddaeebbd8e324e210c51dfe92a598112
LeAries <learies@ya.ru>
2022-04-20 20:28:50 +0300

    Add complete match for search_fields

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -44,7 +44,7 @@ class FollowViewSet(CreateModelMixin, ListModelMixin, viewsets.GenericViewSet):
     queryset = Follow.objects.all()
     serializer_class = FollowSerializer
     filter_backends = (SearchFilter,)
-    search_fields = ('user__username', 'following__username')
+    search_fields = ('=user__username', '=following__username')
 
     def get_queryset(self):
         return Follow.objects.filter(user=self.request.user)
